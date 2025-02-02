99536c3bec9faa9c497e21a0571f7cc3e81a66cd
molli <momo54@gmail.com>
2025-02-02 20:19:34 +0000

    setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,10 +2,10 @@
 set -e  # Stop if any command fails
 
 # Ensure SDKMAN is loaded before using it
-source ~/.sdkman/bin/sdkman-init.sh
+#source ~/.sdkman/bin/sdkman-init.sh
 
 # Install Java
-sdk install java 17.0.8-tem
+#sdk install java 17.0.8-tem
 
 # Install Playwright and Chromium browser
 pip install playwright
